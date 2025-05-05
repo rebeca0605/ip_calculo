ed14f85d513f0c6417512a5610cec613d150fe04
REBECA GOMES DA SILVA <25132910@CFP127ALUNO.LOCAL>
2025-05-05 10:34:37 -0300

    Novo arquivo e novos cálculo ai

---
+++ src/br/dev/rebeca/ip_calculo/CalculoIpApp.java
@@ -0,0 +1,5 @@
+package br.dev.rebeca.ip_calculo;
+
+public class CalculoIpApp {
+
+}
---
+++ src/br/dev/rebeca/ip_calculo/gui/TelaCalculo.java
@@ -0,0 +1,26 @@
+package br.dev.rebeca.ip_calculo.gui;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TelaCalculo {
+
+	public class TelaCalculoIp {
+
+		private JLabel lblEndIp;
+
+		private JLabel lblIp;
+		private JLabel lblClasseIp;
+		private JLabel lblMascaraDecimal;
+		private JLabel lblIpDisponíveis;
+
+		private JTextField txtPrimeiroOcteto;
+		private JTextField txtSegundoOcteto;
+		private JTextField txtTerceiroOcteto;
+		private JTextField txtQuartoOcteto;
+
+		private JButton buttonCalcular;
+	}
+
+}
